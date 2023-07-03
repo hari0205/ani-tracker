8bf5a98ed5258e975170259c730ed9d47d7a28c4
hari0205 <nhariharasudhan7@gmail.com>
2023-07-03 19:21:27 +0530

    Added Auth middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,40 @@
+import { BadRequestException, Injectable, NestMiddleware, UnauthorizedException } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { NextFunction, Request, Response } from "express";
+import { User } from "../user/user.entity";
+import { Repository } from "typeorm";
+import { InjectRepository } from "@nestjs/typeorm";
+
+
+
+
+
+
+
+
+
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
+    constructor(private readonly jwtService: JwtService,
+        @InjectRepository(User) private userRepo: Repository<User>) { }
+    async use(req: Request, res: Response, next: (error?: NextFunction) => void) {
+
+        const token = req.cookies.authoization;
+
+        if (token) {
+
+            try {
+                const decodedToken = await this.jwtService.verifyAsync(token);
+                req.user = await this.userRepo.findOne({ where: { id: decodedToken.id } })
+                next()
+            } catch {
+                throw new BadRequestException('Invalid token signature');
+            }
+
+        }
+
+        throw new UnauthorizedException("No token found")
+
+    }
+
+}
